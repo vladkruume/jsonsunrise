227f94f308c72513d23261dd7d1bcedd8b766d5c
opilane <opilane@HP-CZC5280M5K>
2020-10-13 10:02:26 +0300

    first commit

---
+++ SunriseSunsetAPI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace SunriseSunsetAPI
+{
+    class Program
+    {
+        public static string url = "https://api.sunrise-sunset.org/json?lat=59.4370&lng=24.7536&date=2020-10-13";
+        static void Main(string[] args)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+            using (var responsereader = new StreamReader(webStream))
+            {
+                var response = responsereader.ReadToEnd();
+                SunMode sunMode = JsonConvert.DeserializeObject<SunMode>(response);
+                Console.WriteLine($"sunrise:{sunMode.Results.sunrise}");
+                Console.WriteLine($"sunset:{sunMode.Results.sunset}");
+                Console.WriteLine($"status:{sunMode.status}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ SunriseSunsetAPI/SunMode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SunriseSunsetAPI
+{
+    public class SunMode
+    {
+        public Results Results { get; set;}
+        public string status { get; set;}
+    }
+    public class Results
+    {
+        public string sunrise { get; set;}
+        public string sunset { get; set;}
+    }
+}
